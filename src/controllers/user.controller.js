455d2892ca9fe4df880c020997cf81f2cf23947b
prajapatijay03 <jayp37679@gmail.com>
2024-06-03 17:57:30 +0530

    user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -205,7 +205,7 @@ const logoutUser = asyncHnadler(async (req, res) => {
 const refreshAccessToken = asyncHnadler(async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unauthorized request")
     }
 
