5ca98b4e6bdc6202a9e94032104b6114d356a10a
prajapatijay03 <jayp37679@gmail.com>
2024-06-03 11:10:17 +0530

    post man testing and error solving and data savign in DB

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,9 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUSer = asyncHnadler(async (req, res) => {
 
+
+    // ++++++  Algoritham ++++++
+
     // get user details from frontend 
     // validation -- not empty
     // check if user already exists : username, email
@@ -19,7 +22,7 @@ const registerUSer = asyncHnadler(async (req, res) => {
 
 
     const { fullName, email, username, password } = req.body
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
 
     if (
@@ -28,7 +31,7 @@ const registerUSer = asyncHnadler(async (req, res) => {
         throw new ApiError(400, "all fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -36,8 +39,17 @@ const registerUSer = asyncHnadler(async (req, res) => {
         throw new ApiError(409, "User with email or username is already exists")
     }
 
+    // console.log(req.files);
+
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
